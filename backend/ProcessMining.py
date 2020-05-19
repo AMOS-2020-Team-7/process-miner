13a4149394ba0e72fb762c8a3a14996db8e3b671
AnnaHolzmann <49980371+AnnaHolzmann@users.noreply.github.com>
2020-05-19 18:21:46 +0200

    Create ProcessMining.py

---
+++ backend/ProcessMining.py
@@ -0,0 +1,10 @@
+import os
+
+from pm4py.objects.log.adapters.pandas import csv_import_adapter
+from pm4py.objects.conversion.log import factory as conversion_factory
+from pm4py.algo.discovery.dfg import factory as dfg_factory
+from pm4py.visualization.dfg import factory as dfg_vis_factory
+from pm4py.util import constants
+
+
+print("Hello World!")

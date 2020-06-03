2d893fc448255a519fa0a9248a59545e1ac87072
Johann Schramm <43448334+JohannSchramm@users.noreply.github.com>
2020-06-03 14:08:48 +0200

    Add HttpClientTestingModule

--- frontend/src/app/data.service.spec.ts
+++ frontend/src/app/data.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { DataService } from './data.service';
 
@@ -6,7 +7,9 @@ describe('DataService', () => {
   let service: DataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+    });
     service = TestBed.inject(DataService);
   });
 

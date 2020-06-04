96db2f6f9ed6acf1c4abfbf9e7b90cad4cd38ba6
Markus Büttner <markus.buettner@fau.de>
2020-06-04 16:13:39 +0200

    Deduplicate code and use YAML config file

--- backend/process_miner/__init__.py
+++ backend/process_miner/__init__.py
@@ -3,21 +3,31 @@ Module responsible for initializing the flask app that provides the backend
 service of the process miner.
 """
 import logging
+from pathlib import Path
 
 from flask import Flask
 
+from process_miner.configuration_loader import ConfigurationLoader
 from process_miner.log_retriever import LogRetriever
-from .process_miner_config import log_retriever as log_retriever_cfg
+
+CONFIG_FILENAME = 'process_miner_config.yaml'
 
 log = logging.getLogger(__name__)
 
 logging.basicConfig(level=logging.INFO)
-log.info("setting up log retriever")
-retriever = LogRetriever(log_retriever_cfg["url"],
-                         log_retriever_cfg["api_token"],
-                         log_retriever_cfg["target_dir"]
+
+log.info('setting up configuration')
+cfg_loader = ConfigurationLoader(Path(CONFIG_FILENAME))
+global_cfg = cfg_loader.get_section('global')
+log_retriever_cfg = cfg_loader.get_section('log_retriever')
+
+log.info('setting up log retriever')
+retriever = LogRetriever(log_retriever_cfg['url'],
+                         log_retriever_cfg['api_token'],
+                         global_cfg['log_directory']
                          )
 
+log.info('setting up flask app')
 process_miner_app = Flask(__name__)
 
 
@@ -35,7 +45,7 @@ def refresh_logs():
 def create_app():
     """
     Factory method for creating the Flask object representing the actual
-    flask app
+    flask app.
     :return: the Flask object
     """
     return process_miner_app
--- backend/process_miner/process_miner_config.py
+++
@@ -1,8 +0,0 @@
-"""
-Module esd for configuring the process miner
-"""
-log_retriever = {
-    'url': "INSERT_GRAYLOG_URL_HERE",
-    'api_token': "INSERT_API_TOKEN_HERE",
-    'target_dir': "../retrieved_logs"
-}

7bd85db061d9d80a8abf0636ec6f730a4d2bd717
Hugo Ibanez <hugoibanez@User23.local>
2020-06-08 00:12:06 +0200

    Logs toolbar added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,7 +14,10 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatIconModule} from '@angular/material/icon';
-import { UserComponent } from './user/user.component';
+import {UserComponent } from './user/user.component';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -33,7 +36,10 @@ import { UserComponent } from './user/user.component';
     MatCardModule,
     MatButtonModule,
     MatProgressSpinnerModule,
-    MatIconModule
+    MatIconModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]

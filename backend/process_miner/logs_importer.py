cbdb6793b673918d68a83ffda046bb734356f8c4
rahmanova <rahmanova.jahana@gmail.com>
2020-06-14 13:20:45 +0200

    "Tagged logs imported in heuristic miner"

--- backend/process_miner/logs_importer.py
+++ backend/process_miner/logs_importer.py
@@ -1,5 +1,6 @@
+
 """
-Module used for importing tagged logs into heuristic miner
+Module used for importing tagged logs into heuristic miner from retrieved_logs folder
 """
 import glob
 import pandas as pd
@@ -12,14 +13,17 @@ from pm4py.visualization.heuristics_net import visualizer as hn_vis
 
 
 def concat_files():
+    """
+        all CSV files get concatenated into one file
+    """
     all_filenames = [i for i in glob.glob('retrieved_logs/'+'*.csv')]
-    # combine all files in the list
     combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
-    # export to csv
     combined_csv.to_csv("concated_files.csv", index=False, encoding='utf-8-sig')
 
 def execute_script():
-    # import csv & create log
+    """
+        creates Directly-Follows graph finds common structures, outputs Heuristic Net in .png format
+    """
     dataframe = csv_import_adapter.import_dataframe_from_path('concated_files.csv', sep=",")
     dataframe = dataframe.rename(columns={'correlationId': 'case:concept:name', 'timestamp': 'time:timestamp', 'message': 'concept:name'})
     log = conversion_factory.apply(dataframe)
@@ -27,3 +31,4 @@ def execute_script():
     heu_net = heuristics_miner.apply_heu(log, parameters={heuristics_miner.Variants.CLASSIC.value.Parameters.DEPENDENCY_THRESH: 0.00})
     gviz2 = hn_vis.apply(heu_net, parameters={hn_vis.Variants.PYDOTPLUS.value.Parameters.FORMAT: "png"})
     hn_vis.view(gviz2)
+

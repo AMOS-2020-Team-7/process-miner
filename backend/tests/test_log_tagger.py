f087babfa468f3097f58a85051b1f1463507ee5f
Markus Büttner <markus.buettner@fau.de>
2020-06-15 20:44:31 +0200

    Add tests for the log_tagger module

---
+++ backend/tests/test_log_tagger.py
@@ -0,0 +1,88 @@
+"""
+Tests for the log_tagger module
+"""
+import copy
+
+from process_miner.log_tagger import LogTagger
+
+DEFAULT_VALUE = 'default'
+
+
+def _create_taggers():
+    # will only tag matching entries
+    tagger_single = LogTagger('source', 'target_single', False, DEFAULT_VALUE)
+    tagger_single.add_mapping('single_tagger_value',
+                              [
+                                  'single'
+                              ])
+    # will tag all entries on match
+    tagger_all = LogTagger('source', 'target_all', True, DEFAULT_VALUE)
+    tagger_all.add_mapping('all_tagger_value',
+                           [
+                               'all'
+                           ])
+
+    return [tagger_single, tagger_all]
+
+
+def _tag_entries(entries):
+    for tagger in _create_taggers():
+        tagger.tag_entries(entries)
+
+
+def test_tag_entries_no_matches():
+    """
+    Checks if default values get inserted properly if no entry matches.
+    """
+    entries = [
+        {'unrelated_field': 'value', 'source': 'some value'},
+        {'unrelated_field': 'value', 'source': 'another value'}
+    ]
+
+    expected_entries = copy.deepcopy(entries)
+    for entry in expected_entries:
+        entry['target_single'] = DEFAULT_VALUE
+        entry['target_all'] = DEFAULT_VALUE
+
+    _tag_entries(entries)
+
+    assert entries == expected_entries
+
+
+def test_tag_entries_single_matches():
+    """
+    Checks if single entry matcher inserts its values properly
+    """
+    entries = [
+        {'unrelated_field': 'value', 'source': 'single'},
+        {'unrelated_field': 'value', 'source': 'sing but no le'}
+    ]
+
+    expected_entries = copy.deepcopy(entries)
+    expected_entries[0]['target_single'] = 'single_tagger_value'
+    expected_entries[1]['target_single'] = DEFAULT_VALUE
+    for entry in expected_entries:
+        entry['target_all'] = DEFAULT_VALUE
+
+    _tag_entries(entries)
+
+    assert entries == expected_entries
+
+
+def test_tag_entries_all_matches():
+    """
+    Checks if all entries get tagged properly if tag_all was enabled.
+    """
+    entries = [
+        {'unrelated_field': 'value', 'source': 'all'},
+        {'unrelated_field': 'value', 'source': 'some'}
+    ]
+
+    expected_entries = copy.deepcopy(entries)
+    for entry in expected_entries:
+        entry['target_single'] = DEFAULT_VALUE
+        entry['target_all'] = 'all_tagger_value'
+
+    _tag_entries(entries)
+
+    assert entries == expected_entries

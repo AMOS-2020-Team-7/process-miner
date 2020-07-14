919b6b4b8ecddf28ad1cd7757ddb71041ac8aa7c
Hugo Ibanez <hugoibanez@gmail.com>
2020-07-14 18:26:00 +0200

    processes view is now the landing page and user page was removed

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProcessesComponent } from './processes/processes.component';
-import { UserComponent } from './user/user.component';
 
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '', redirectTo: 'processes', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'processes', component: ProcessesComponent },
-  { path: 'user', component: UserComponent },
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,7 +13,6 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatIconModule} from '@angular/material/icon';
-import {UserComponent } from './user/user.component';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
@@ -29,7 +28,6 @@ import { GraphDisplayComponent } from './graph-display/graph-display.component';
     AppComponent,
     HomeComponent,
     ProcessesComponent,
-    UserComponent,
     GraphDisplayComponent
   ],
   imports: [
